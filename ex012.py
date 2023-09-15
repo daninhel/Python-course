35104d7f351a092a093dfd99b7d9ecd5ba4c17cf
Daniel <69803640+daninhel@users.noreply.github.com>
2023-09-15 17:14:18 -0300

    Add files via upload

---
+++ ex012.py
@@ -0,0 +1,19 @@
+from os import system as cmd
+
+verifica = False
+
+while not verifica :
+    #recolhe os dados
+    num = input('Digite um numero para exibir sua taboada: ')
+    contador = input('Digite até qual número você quer a taboada do numero anterior: ')
+    cmd('cls')
+    if num.isdigit() and contador.isdigit() :
+        auxiliar = 0
+        verifica = True
+        while auxiliar <= int(contador) :
+            print(f"{auxiliar} X {int(num)} = {int(num) * auxiliar}")
+            auxiliar += 1
+    else :
+            verifica = False
+#saida do programa
+cmd('pause')
