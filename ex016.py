22917a2d387a89c42ad1712f379ce8fc58c0a301
Daniel <69803640+daninhel@users.noreply.github.com>
2023-09-27 16:06:41 -0300

    Add files via upload

--- ex016.py
+++ ex016.py
@@ -2,56 +2,51 @@ from os import system as cmd
 from time import sleep as wait
 
 def rerrada():
-    cmd('cls')
-    print('Digites apenas valores correspondentes ao menu.')
-    wait(3)
-    cmd('cls')
+    cmd('cls'), print('Digites apenas valores correspondentes ao menu.'), wait(3), cmd('cls')
 
-i = 0
-clientes = [{"nome": [], 
-             "email": []}]
-aux = True
+i , aux, clientes = 0 , True, {"nome": [], "email": [],"endereco":[]}
 
 while aux:
-    #primeiro menu
-    print('1 - Cadastrar clientes \n2 - Listar clientes\n3 - Sair')
+    cmd('cls')
+    print('[1] - Cadastrar clientes\n[2] - Listar clientes\n[3] - Encerrar programa')
     a = None
-    try :
+    try:
         a = input('')
-    except KeyboardInterrupt :
+    except KeyboardInterrupt:
         pass
+
     if a == '1' :
-        #cadatrar clientes
-        cmd('cls')
-        print('1 - Cadastrar\n2 - Voltar\n3 - Encerrar')
-        try :
-            a = input('')
-        except KeyboardInterrupt :
-            pass
-        if a == '1' :
-            while True :
+        while True:
+            cmd('cls'), print('[1] - Cadastrar\n[2] - Voltar\n[3] - Encerrar')
+            try:
+                a = input('')
+            except KeyboardInterrupt:
+                pass
+            if a == '1':
                 cmd('cls')
-                clientes.nome[i] = input('Digite o nome : ')
-                clientes.email[i] = input('Digite o email : ')
+                clientes['nome'].append(input('Digite o nome: '))
+                clientes['email'].append(input('Digite o email: '))
+                clientes['endereco'].append(input('Digite o endereço: '))
                 cmd('cls')
+                print('Cliente cadastrado com sucesso !')
+                wait(3)
                 i += 1
+            elif a == '2':
+                cmd('cls')
                 break
-        elif a == '2' :
-            cmd('cls')
-            continue
-        elif a == '3' :
-            aux = False
-        else:
-            rerrada()
+            elif a == '3':
+                cmd('cls') , print('Encerrando . . .') , wait(3)
+                aux = False
+            else:
+                rerrada()            
     elif a == '2' :
-        #Listar clientes
-        cmd('cls')
-        print(clientes.nome)
-        print(clientes.numero)
+        cmd('cls'), print(f'Você tem {i} clientes cadastrados')
+        if i != 0 :
+            for x in range(i):
+                print(f"Cliente {x+1} Nome: {clientes['nome'][x]}, Email: {clientes['email'][x]}, Endereço: {clientes['endereco'][x]}")
+        cmd('pause')
     elif a == '3' :
-        #sair
-        cmd('cls')
-        print('Até logo. . .')
+        cmd('cls'), print('Encerrando . . .'), wait(3)
         aux = False
-    else :
+    else:
         rerrada()
